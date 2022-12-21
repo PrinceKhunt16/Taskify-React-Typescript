136fb9b997d0ea75b53ab35c51c7633c3173ef81
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-21 08:35:08 +0530

    User authentication backend done

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,47 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const generateToken = require('../utils/generateToken')
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, password } = req.body
+
+  const userExists = await User.findOne({ name })
+
+  if(userExists){
+    res.status(400)
+    throw new Error('The name should be unique. This name already taken.')
+  }
+
+  const user = await User.create({
+    name, 
+    password
+  })
+
+  if(user){
+    res.status(201).json({
+      name,
+      token: generateToken(user.name)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Error ocurred.')
+  }
+})
+
+const authUser = asyncHandler(async (req, res) => {
+  const { name, password } = req.body
+
+  const user = await User.findOne({ name })
+  
+  if(user && (await user.matchPassword(password))){
+    res.json({
+      name: user.name,
+      token: generateToken(user.name)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid name or password.')
+  }
+})
+
+module.exports = { registerUser, authUser }
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,15 @@ const express = require('express')
 require('dotenv').config('')
 const app = express()
 const connectDB = require('./db/connect')
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
+const userRoutes = require('./routes/userRoutes')
 
 connectDB()
+app.use(express.json())
+
+app.use('/api/users', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
   console.log('Server Running')
